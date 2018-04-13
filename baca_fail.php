449b90ec8597e568e50c383e6905de70e212cacc
amin007 <helmi007@yahoo.com>
2018-04-14 02:48:37 +0800

    Add files via upload

---
+++ baca_fail.php
@@ -0,0 +1,40 @@
+<?php
+function dirToArray($dir)
+{ 
+	$result = array(); 
+	$cdir = scandir($dir); 
+
+	foreach ($cdir as $key => $value) 
+	{ 
+		if (!in_array($value,array(".",".."))) 
+		{ 
+			if (is_dir($dir . DIRECTORY_SEPARATOR . $value))
+			{ 
+				$result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+			} 
+			else 
+			{ 
+				$result[] = $value;
+			} 
+		} 
+	} 
+
+	return $result; 
+} 
+# https://www.programmableweb.com/news/150-translation-apis-google-translate-merriam-webster-and-microsoft-translator/2018/03/22
+# C:\xampp\htdocs\belajar\baca-fail\srt\MrRobot\MrRobotSeason01
+$dir1 = './MrRobot';
+$dir2 = '/MrRobot';
+$files = dirToArray($dir1);
+include 'class/bacaSrt.php';
+
+//echo '<pre>';//print_r($files1);
+$k = 1;
+foreach($files as $key => $file1):
+	foreach($file1 as $key2 => $file):
+	//echo '<hr size=7>' . $key2 . ':' . $dir1 . '/' . $key . '/' . $file . '<hr>';
+	echo '<hr size=7>' . $k++ . ':' . $dir1 . '/' . $key . '/' . $file . '<hr>';
+	//$papar = new bacaSrt;
+	//$papar->mulaBaca($dir1 . '/' . $key . '/' . $file);
+endforeach;
+endforeach;//*/
---
+++ class/bacaSrt.php
@@ -0,0 +1,95 @@
+<?php
+
+# https://stackoverflow.com/questions/11659118/parsing-srt-files
+define('SRT_STATE_SUBNUMBER', 0);
+define('SRT_STATE_TIME',      1);
+define('SRT_STATE_TEXT',      2);
+define('SRT_STATE_BLANK',     3);
+
+class bacaSrt
+{
+#------------------------------------------------------------------------------------------------------#
+	//function __construct($dir)
+	public function mulaBaca($dir)
+	{
+		//$lines   = file('test.srt');
+		# C:\xampp\htdocs\belajar\baca-fail\srt\Mr Robot\Mr. Robot - season 1
+		$lines   = file("$dir");
+		$subs    = array();
+		$state   = SRT_STATE_SUBNUMBER;
+		$subNum  = 0;
+		$subText = $subTime = '';
+
+		$subs = $this->bacaLines($lines, $subs, $state, $subNum, $subText, $subTime);
+		$this->bacaSubs($subs);
+	}
+#------------------------------------------------------------------------------------------------------#
+	public function bacaLines($lines, $subs, $state, $subNum, $subText, $subTime)
+	{		
+		foreach($lines as $line) 
+		{
+			##-----------------------------------------------------------------------------
+			switch($state) 
+			{
+				case SRT_STATE_SUBNUMBER:
+					$subNum = trim($line);
+					$state  = SRT_STATE_TIME;
+					break;
+				case SRT_STATE_TIME:
+					$subTime = trim($line);
+					$state   = SRT_STATE_TEXT;
+					break;
+				case SRT_STATE_TEXT:
+					if (trim($line) == '') 
+					{
+						$sub = new stdClass;
+						$sub->number = $subNum;
+						list($sub->startTime, $sub->stopTime) = explode(' --> ', $subTime);
+						$sub->text   = $subText;
+						$subText     = '';
+						$state       = SRT_STATE_SUBNUMBER;
+							
+						$subs[]      = $sub;
+					} else {
+						//$subText .= $line;
+						$subText .= htmlspecialchars($line, ENT_QUOTES);
+					}
+					break;
+			}
+			##-----------------------------------------------------------------------------
+			//list($subs, $state, $subNum, $subText, $subTime, $sub) =
+			//	$this->pilihLines($line, $subs, $state, $subNum, $subText, $subTime);
+		}
+		#------------------------------------------------------------------------------------------------------#
+		if ($state == SRT_STATE_TEXT) {
+			// if file was missing the trailing newlines, we'll be in this
+			// state here.  Append the last read text and add the last sub.
+			$sub->text = $subText;
+			$subs[] = $sub;
+		}//*/
+		#------------------------------------------------------------------------------------------------------#
+
+		return $subs;
+	}
+#------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------#
+	public function pilihLines($line, $subs, $state, $subNum, $subText, $subTime)
+	{
+			
+		return array($subs, $state, $subNum, $subText, $subTime, $sub);
+	}
+#------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------#
+	public function bacaSubs($subs)
+	{
+		//echo '<pre>'; print_r($subs); echo '</pre>';
+		foreach($subs as $sub) 
+		{
+			//echo $sub->number . ' begins at ' . $sub->startTime .
+			//' and ends at ' . $sub->stopTime . '.  The text is: <br /><pre>' .
+			//$sub->text . "</pre><br />\n";
+			echo $sub->number . ':' . $sub->text . "<br>\n";
+		}
+	}
+#------------------------------------------------------------------------------------------------------#
+}
