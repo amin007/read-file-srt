9713378a7d28597af8fd8f1fb7841708fd1ce6d1
amin007 <helmi007@yahoo.com>
2018-04-14 02:52:09 +0800

    ringkaskan koding

--- baca_fail.php
+++ baca_fail.php
@@ -9,13 +9,9 @@ function dirToArray($dir)
 		if (!in_array($value,array(".",".."))) 
 		{ 
 			if (is_dir($dir . DIRECTORY_SEPARATOR . $value))
-			{ 
 				$result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
-			} 
 			else 
-			{ 
 				$result[] = $value;
-			} 
 		} 
 	} 
 
@@ -24,17 +20,16 @@ function dirToArray($dir)
 # https://www.programmableweb.com/news/150-translation-apis-google-translate-merriam-webster-and-microsoft-translator/2018/03/22
 # C:\xampp\htdocs\belajar\baca-fail\srt\MrRobot\MrRobotSeason01
 $dir1 = './MrRobot';
-$dir2 = '/MrRobot';
 $files = dirToArray($dir1);
 include 'class/bacaSrt.php';
 
-//echo '<pre>';//print_r($files1);
+//echo '<pre>';//print_r($files);
 $k = 1;
 foreach($files as $key => $file1):
 	foreach($file1 as $key2 => $file):
 	//echo '<hr size=7>' . $key2 . ':' . $dir1 . '/' . $key . '/' . $file . '<hr>';
 	echo '<hr size=7>' . $k++ . ':' . $dir1 . '/' . $key . '/' . $file . '<hr>';
-	//$papar = new bacaSrt;
-	//$papar->mulaBaca($dir1 . '/' . $key . '/' . $file);
+	$papar = new bacaSrt;
+	$papar->mulaBaca($dir1 . '/' . $key . '/' . $file);
 endforeach;
 endforeach;//*/
