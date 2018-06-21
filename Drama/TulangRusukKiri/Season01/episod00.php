8d9b4b3979909de941755a88fb23f6bb681088d4
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-06-22 01:54:11 +0800

    Pengenalan kepada drama terbaru. Traler katanya.

---
+++ Drama/TulangRusukKiri/Season01/episod00.php
@@ -0,0 +1,9 @@
+Sinopsis Tulang Rusuk Kiri
+Pengembaraan hero mencari cinta hatinya
+Gunakan konsep oop aka pengaturcaraan berasaskan objek
+dalam menerangkan programming kepada orang awam.
+
+satu episod 5 minit
+5 minit * 60 saat = 300 saat
+satu line skrip dalam 10 saat
+jadi kena buat 30 line skrip untuk dapatkan 300 saat punya cerita
