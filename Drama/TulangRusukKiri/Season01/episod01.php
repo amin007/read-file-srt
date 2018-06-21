7885d34a30621730bc95be3876aa914fc419181a
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-06-22 01:54:41 +0800

    episod satu season 1

---
+++ Drama/TulangRusukKiri/Season01/episod01.php
@@ -0,0 +1,34 @@
+Perjalanan hidup ini
+
+01. Dalam hidup ini, hero hanya seorang diri di dunia yang luas.
+02. Ikutkan boleh sahaja hidup seorang diri tanpa libarkan orang lain.
+03. Terkadang kala cemburu orang lain ada pasangannya.
+04. Pernah menulis surat cinta sepanjang 4 helai kertas A4
+05. Beli hadiah keronsang berwarna hijau import dari luar negara
+06. Namun ditolak mentah-mentah oleh orang tersebut.
+07. 3 tahun menunggu mengarang surat cinta namun akhirnya ditolak.
+08. Pernah juga rapat dengan seseorang sampai jadi BFF
+09. Namun hanya sekarang teman tetapi mesra sahaja
+10. Tidak boleh pergi lebih jauh dari itu
+11. Kerana nanti akan hilang peluang untuk sesuatu yang lagi penting dari cinta
+12. Oh ada yang lebih penting dari cinta ya?
+13. Ya, antaranya pelajaran, ilmu, internet percuma dan sebagainya
+14. Yang penting orang yang kita suka duduk sebelah walaupun kita tidak dapat hatinya
+15. Bahagia ker macam itu? Bukankah ia hanya menambah luka di hati?
+16. Definasi suka,sayang,cinta,bahagia,seks berbeza bagi diri ini.
+17. Suka bermaksud ...
+18. Sayang bermaksud ...
+19. Cinta bermaksud ..
+20. Bahagia bermaksud ...
+21. Seks bermaksud ...
+22. Namun hanya kepada Allah dan Rasul tempat mengadu.
+33. Solat dapat menenangkan hati yang lara
+24. Hadis nabi didengar setiap subuh dan asar.
+25. Mendengar cinta nabi kepada umat dari besar dari cinta sesama manusia.
+26. Keluar bermusafir dari masjid ke masjid.
+27. Mungkin dalam perjalanan ini akan bertemu cinta abadi dan cinta hati.
+28. Itulah dulu di sekolah ada yang berminat dengan diri ini.
+29. Tetapi diri ini lari jauh-jauh dari orang tersebut.
+30. Sampai orang itu patah hati kerana diri ini.
+
+Bersambung ...
