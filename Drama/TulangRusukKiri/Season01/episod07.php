020cb7403e8b18633b2ee0ce689aeec0a6d9bc08
Amin007 Ledang <helmi007@yahoo.com>
2018-07-02 10:54:10 +0800

    tambah cerita sikit

--- Drama/TulangRusukKiri/Season01/episod07.php
+++ Drama/TulangRusukKiri/Season01/episod07.php
@@ -1,45 +1,52 @@
 Taip kelas Papar
 
-
 Hero : ok dalam class papar ada 3 fungsi iaitu
-pilihTemplate
-paparTemplate
-bacaTemplate
+pilihTemplate, paparTemplate dan bacaTemplate
 
 pilihTemplate untuk kita pilih template sedia ada dalam sistem
 paparTemplate untuk debug lokasi template di mana
 manakala bacaTemplate untuk masukkan semua 
 
-Amoi : ini kena taip atau salin-tampal.
-Hero : ok kita salin di https://github.com/amin007/mvc-melayu/blob/master/Aplikasi/Kelas/Kitab/Papar.php
-
-<?php
-namespace Aplikasi\Kitab; //echo __NAMESPACE__; 
-class Papar
-{
-#==========================================================================================
-	function __construct()
-	{
-		//echo '<br>1. Anda berada di class Papar<br>';
-	}
-#-----------------------------------------------------------------------------------------
-	function pilihTemplate($template = 0)
-	{
+Amoi : baik, saya faham sikit-sikit-si
+Hero : ok sekarang taip 
+Taip -lebih besar-soalan-php-[enter]
+Amoi : -<-?-php-[enter]
+ok lepas itu taip apa?
+Hero : kita taip 
+-namespace-Aplikasi-\-Kitab-coma bertitik-[ruang]-//echo-[ruang]-__NAMESPACE__;-[enter]
+Amoi :
+-namespace-Aplikasi-\-Kitab-;-[ruang]-//echo-[ruang]-__NAMESPACE__;-[enter]
+Hero : taip class-[ruang]-Papar-[enter]-{-[enter]
+Amoi : taip class-[ruang]-Papar-[enter]-{-[enter]
+Hero : lepas itu buat garisan panjang kalulistiwa
+-#==========================================================================================
+Amoi : -#==========================================================================================
+Hero : ok, sekarang buat fungsi __construct.
+Taip -[tab]-function __construct()-[enter]-[tab]-{-[enter]
+Amoi : -[tab]-function __construct()-[enter]-[tab]-{-[enter]
+Hero : lepas itu taip -[tab]//echo '<br>1. Anda berada di class Papar<br>';-[enter]-[tab]-}-[enter]
+Amoi : -[tab]//echo '<br>1. Anda berada di class Papar<br>';-[enter]-[tab]-}-[enter]
+Hero : lepas itu buat garisan panjang kalulistiwa
+-#-----------------------------------------------------------------------------------------
+Amoi : -#-----------------------------------------------------------------------------------------
+Hero : sekarang kita sudah buat fungsi __construct, lepas ini buat class pilihTemplate
+taip -[tab]function pilihTemplate($template = 0)-[enter]-[tab]-{-[enter]
+Amoi : -[tab]function pilihTemplate($template = 0)-[enter]-[tab]-{-[enter]
 		switch ($template)
 		{
-			case 5: $jenis = 'miminium';
+			case 5: $jenis = 'A5';
 			break;
 
-			case 4: $jenis = 'modern_admin_panel';
+			case 4: $jenis = 'A4';
 			break;
 
-			case 3: $jenis = 'startbootstrap-sb-admin-2-1.0.8';
+			case 3: $jenis = 'A3';
 			break;
 
-			case 2: $jenis = 'FlatAdmin-V.2';
+			case 2: $jenis = 'A2';
 			break;
 
-			case 1: $jenis = 'AdminLTE-2.3.0';
+			case 1: $jenis = 'A1';
 			break;
 
 			default: $jenis = 'A0';
