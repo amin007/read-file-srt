fb665a2ed12079eee2f6bbf679ca114ae040056a
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-02-08 16:05:56 +0800

    tambah skrip

--- Drama/TulangRusukKiri/Season01/episod09.php
+++ Drama/TulangRusukKiri/Season01/episod09.php
@@ -3,10 +3,11 @@ Konsep pembolehubah
 Awek1 : Nanti ada kawan baru masuk rumah sewa kita ini
 Awek2 : Bila dia akan masuk?
 Awek1 : kata tuan rumah 1 jam lagi
-Awek2 : oh kita kena sambut rakan baru ini
-05. 
-06. 
-07. 
+Awek2 : oh kita kena sambut rakan baru ini, namanya siapa ya?
+Awak1 : entahlah lupa hendak tanya tuan rumah, nanti dia datang kita tanyalah
+Awek2 : baiklah kita bersiap2 tunggu kedatangan dia
+Awek3 : Assalamualaikum.
+Awek1 : Waalaikumusalam, masuklah.
 08. 
 09. 
 10. 
