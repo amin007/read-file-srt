c645e066802300162a199a37e6835d039016e898
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-10-14 19:10:05 +0800

    ringkasan sinopsis drama Mr Robot versi melayu daa

--- Drama/TulangRusukKiri/Season01/episod00.php
+++ Drama/TulangRusukKiri/Season01/episod00.php
@@ -7,3 +7,148 @@ satu episod 5 minit
 5 minit * 60 saat = 300 saat
 satu line skrip dalam 10 saat
 jadi kena buat 30 line skrip untuk dapatkan 300 saat punya cerita
+
+#-------------------------------------------------------------------------------------------------
+# episod 01 - Perjalanan hidup ini
+
+Sinopsis : episod kali ini menerangkan sifat hero katanya
+Jenis monolog aka hero bercakap dalam hati.
+
+# Skrip
+Dalam hidup ini, hero hanya seorang diri di dunia yang luas.
+Ikutkan boleh sahaja hidup seorang diri tanpa libarkan orang lain.
+Terkadang kala cemburu orang lain ada pasangannya.
+...
+
+Bersambung...
+#-------------------------------------------------------------------------------------------------
+# episod 02 - Konsep programming
+
+Sinopsis : episod kali ini menerangkan konsep programming secara asasnya.
+Jenis monolog aka hero bercakap dalam hati.
+
+# Skrip
+Hidup ini terikat dengan peraturan dunia dan akhirat.
+Banyak undang-undang yang perlu dipatuhi.
+Ada kalanya undang-undang akan dilanggar.
+Ada kalanya undang-undang akan diubah atau digubal mengikut peredaran masa
+...
+
+Bersambung...
+#-------------------------------------------------------------------------------------------------
+# episod 03 - Konsep fail
+Sinopsis : episod kali ini menerangkan konsep fail secara asasnya.
+tempat berlaku dalam pejabat hero daa
+Jenis monolog aka hero bercakap dalam hati.
+
+# Skrip - belum ada idea!!!
+
+Bersambung ...
+#-------------------------------------------------------------------------------------------------
+# episod 03 - Konsep folder
+Sinopsis : episod kali ini menerangkan konsep folder secara asasnya.
+tempat berlaku dalam kedai buku/perpuskataan
+Jenis monolog aka hero bercakap dalam hati.
+
+# Skrip - belum ada idea!!!
+
+Bersambung ...
+#-------------------------------------------------------------------------------------------------
+# episod 05 - Konsep pembolehubah
+
+Sinopsis : episod kali ini menerangkan konsep pembolehubah secara asasnya.
+tempat berlaku dalam rumah heroin dengan kawan-kawannya.
+
+# Skrip
+Awek1 : Nanti ada kawan baru masuk rumah sewa kita ini
+Awek2 : Bila dia akan masuk?
+Awek1 : kata tuan rumah 1 jam lagi
+Awek2 : oh kita kena sambut rakan baru ini, namanya siapa ya?
+...
+
+Bersambung ...
+#-------------------------------------------------------------------------------------------------
+# episod 06 - Konsep tatasusunan/array
+
+Sinopsis : episod kali ini menerangkan konsep pembolehubah secara asasnya.
+tempat berlaku dalam rumah heroin dengan kawan-kawannya.
+
+# Skrip
+Awek3 : {awek2}, awak boleh temankan kita pergi kedai perabut?
+Awek2 : awak nak cari apa dalam kedai perabut?
+Awek3 : kita hendak cari almarilah, nak susun baju-baju kita.
+Awek2 : kalau begitu ajak {awek1} sekali macam itu
+Awek1 : {awek3}, nak pergi bila
+...
+
+Bersambung...
+#-------------------------------------------------------------------------------------------------
+# episod 07 - Konsep function
+
+Sinopsis : episod kali ini menerangkan konsep function secara asasnya.
+tempat berlaku dalam rumah heroin dengan kawan-kawannya.
+
+# Skrip
+Awek3 : banyak betul peralatan dalam dapur rumah ini, tak tahu nak guna macam mana
+Awek1 : kami suka memasak, setiap peralatan sesuai dengan resepi makanan tertentu
+Awek2 : ya, sudu Avon pun ada macam2 jenis, sesuai dengan makanan tertentu
+Awek3 : awak berdua boleh terangkan penggunaan peralatan tersebut
+Awek2 : kejap nak cari manual
+...
+
+Bersambung...
+#-------------------------------------------------------------------------------------------------
+# episod 08 - Konsep penganturcaraan berasaskan objek
+
+Sinopsis : episod kali ini menerangkan konsep penganturcaraan berasaskan objek secara asasnya.
+heroin berbual dengan rakan dalam rumah tentang pokok bunga jiran depan rumah.
+
+Awek3 : banyak pokok bunga dekat rumah depan itu ya?
+Awek1 : makcik depan itu memang rajin tanam pokok bunga.
+Awek2 : nanti ada masa kita pergi rumah makcik itu ya lihat pokok bunga
+Awek3 : bila hendak pergi ya?
+Awek1 : kita pergi hujung minggu boleh?
+...
+
+Bersambung...
+#-------------------------------------------------------------------------------------------------
+# episod 09 - Konsep class
+
+Sinopsis : episod kali ini menerangkan konsep class secara asasnya.
+tempat berlaku dalam rumah heroin dengan kawan-kawannya.
+mereka tengah fikir hendak masak apa pada hari minggu.
+masing2 akan tentukan tugas yang perlu dilakukan
+
+# Skrip
+Awek1 : hari minggu ini rasanya hendak masak apa ya?
+Awek2 : masak kek nak?
+Awek3 : kek apa hendak masak?
+
+Bersambung...
+#-------------------------------------------------------------------------------------------------
+# episod 10 - Konsep framework
+
+Sinopsis : episod kali ini menerangkan konsep framework secara asasnya.
+dalam dunia pengaturcaraan, kita ada konsep mcv iaitu model,control dan view.
+dalam bab ini heroin akan berjumpa dengan hero katanya.
+tetapi hanya berselisih sahaja, ceh dramalah sangat
+
+# Skrip - belum ada idea!!!
+
+Bersambung...
+#-------------------------------------------------------------------------------------------------
+# episod 11 - Konsep route
+Sinopsis : episod kali ini menerangkan konsep route dalam framework secara asasnya.
+Dalam bab ini, heroin ajak kawan2 teman pergi bandar dan kebetulan berjumpa dengan hero.
+
+# Skrip
+Awek3 : awak boleh teman kita jalan2 area bandar ini?
+Awek2 : awak mahu pergi mana?
+
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
