772285446f60f72e058245e2875e12d155f7f250
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-10-14 10:02:45 +0800

    tambah konsep pengaturcaraan berasaskan objek

--- Drama/TulangRusukKiri/Season01/episod14.php
+++ Drama/TulangRusukKiri/Season01/episod14.php
@@ -1,9 +1,10 @@
-Php - people help people
+Konsep penganturcaraan berasaskan objek
 
-01. 
-02. 
-03. 
-04. 
+Awek3 : banyak pokok bunga dekat rumah depan itu ya?
+Awek1 : makcik depan itu memang rajin tanam pokok bunga.
+Awek2 : nanti ada masa kita pergi rumah makcik itu ya lihat pokok bunga
+Awek3 : bila hendak pergi ya?
+Awek1 : kita pergi hujung minggu boleh?
 05. 
 06. 
 07. 
