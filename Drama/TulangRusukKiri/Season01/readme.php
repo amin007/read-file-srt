3e01ab4d665d5eb31483ee1f3259a25f7dada621
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-10-21 13:46:10 +0800

    tambah element animasi

--- Drama/TulangRusukKiri/Season01/readme.php
+++ Drama/TulangRusukKiri/Season01/readme.php
@@ -12,6 +12,9 @@ Gunakan konsep oop aka pengaturcaraan berasaskan objek dalam menerangkan konsep 
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+
+<progress id="dynamic" max="300" value="0"></progress>
+
 <?php
 # https://github.com/erusev/parsedown
 include '../../../function/Parsedown.php';
@@ -31,5 +34,13 @@ https://hackerthemes.com/bootstrap-cheatsheet/
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+const elem = document.getElementById('dynamic');
+setInterval(() =>
+{
+	elem.value+=1;
+	if (elem.value === 300) { elem.value = 0; }
+}, 20);
+</script>
 </body>
 </html>
