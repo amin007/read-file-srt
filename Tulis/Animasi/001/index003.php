2edea84b39fe992279378a316b0f951a96441040
Amin Di PC HP Elite Tower 800 G9 <al.amin007@yahoo.com>
2025-11-25 13:14:46 +0800

    tukar bootstarp 5.3.8 dan tukar pembolehubah dalam bahasa melayu

---
+++ Tulis/Animasi/001/index003.php
@@ -0,0 +1,90 @@
+<?php
+$tajukBuku = 'Pengenalan Kepada ...';
+$mukaSebelum = 'Sebelum';//'Previous page';
+$mukaSelepas = 'Selepas';//'Next page';
+//$mukaSurat = [1,2,3,4,5,6];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<link rel="stylesheet" href="static/demoStyle3.css">
+
+<title><?php echo $tajukBuku ?></title>
+</head>
+<body>
+
+<div class="mb-3 mt-2">
+<!-- ========================================================================================== -->
+	<div class="container">
+		<h4 style="text-transform: uppercase"><?php echo $tajukBuku ?></h4>
+		<div class="row">
+		<div class="col-md-7">
+			<div class="row mt-2">
+				<div class="col-md-6" style="min-width:300px">
+					<button type="button" class="btn btn-info btn-sm btn-prev"><?php echo $mukaSebelum ?></button>
+					[<span class="page-current">1</span> dari <span class="page-total">-</span>]
+					<button type="button" class="btn btn-info btn-sm btn-next"><?php echo $mukaSelepas ?></button>
+				</div><!-- / class="col-md-6" style="min-width: 300px" -->
+				<div class="col-md-6">Keadaan: <i class="page-state">read</i>,
+					Kedudukan: <i class="page-orientation">landscape</i></div>
+			</div><!-- / class="row mt-2" -->
+		</div><!-- / class="col-md-7" -->
+		</div><!-- / class="row" -->
+	</div><!-- / class="row" -->
+<!-- ========================================================================================== -->
+<!-- # kandungan | demoBookExample -->
+<!-- ========================================================================================== -->
+	<div class="container-md" style="position: relative">
+	<div class="flip-book html-book demo-book" id="demoBookExample" style="background-image: url('images/background.jpg')">
+	<!-- ========================================================================================== -->
+		<section class="page page-cover page-cover-top" data-density="hard">
+			<div class="page-content">
+				<h2>TAJUK BUKU</h2>
+			</div><!-- / class="page-content" -->
+		</section><!-- /  class="page" -->
+	<!-- ========================================================================================== --><?php
+for($mukaSurat = 1; $mukaSurat <= 10; $mukaSurat++):
+echo "\r\n\t\t"; ?><section class="page">
+			<div class="page-content">
+				<h2 class="page-header">Bab <?php echo $mukaSurat ?></h2>
+				<div class="page-image" style="background-image: url(images/html/<?php echo $mukaSurat ?>.jpg)"></div>
+				<div class="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus, accumsan eros sed, viverra enim. Pellentesque non justo vel nibh sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna.
+				</div><!-- / class="page-text" -->
+				<div class="page-footer"><?php echo ($mukaSurat+1) ?></div>
+			</div><!-- / class="page-content" -->
+		</section><!-- /  class="page" -->
+	<!-- ========================================================================================== --><?php
+endfor; echo "\r\n"; ?>
+		<section class="page page-cover page-cover-bottom" data-density="hard">
+			<div class="page-content">
+				<h2>SUDAH TAMAT</h2>
+  			</div><!-- / class="page-content" -->
+		</section><!-- /  class="page" -->
+	<!-- ========================================================================================== -->
+	</div><!-- / class="flip-book html-book demo-book" -->
+    </div><!-- / class="container-md" style="position: relative" -->
+<!-- ========================================================================================== -->
+</div><!-- / class="mb-3 mt-2" -->
+
+<!-- ========================================================================================== -->
+<!-- Place this tag in your head or just before your close body tag. -->
+<script>
+const pageFlip = new PageFlip(document.getElementById('tajukBuku'),
+    {
+        width: 400, // required parameter - base page width
+        height: 600,  // required parameter - base page height
+        showCover: true
+    }
+);
+</script>
+<script src="static/pagesDemo3.bundle.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+<!-- ========================================================================================== -->
+<!-- nota kaki -->
+</body>
+</html>
